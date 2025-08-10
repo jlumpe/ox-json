d28a30b2ff1974a4a8aab1cf5d0894a7f3428fc5
Jared Lumpe <jared@jaredlumpe.com>
2025-08-09 23:00:14 -0700

    Reduce ridiculous printing in backtrace of test export script

--- tests/export.el
+++ tests/export.el
@@ -6,8 +6,10 @@
 
 (let (
     (ext-plist (list
-      :json-strict (equal (getenv "EXPORT_STRICT") "1")
-    ))
+      :json-strict (equal (getenv "EXPORT_STRICT") "1")))
+    ; Reduce ludicrously long printing of some arguments to export functions in backtraces
+    (print-level 3)
+    (print-length 10)
   )
   (with-current-buffer (find-file-noselect "tests/test.org")
     (org-export-to-file 'json "test.json" nil nil nil nil ext-plist)
